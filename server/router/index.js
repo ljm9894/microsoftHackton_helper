3b51751a8928f41ff9ca66940b18fca079c53c6a
재명 <ljm9894@dgsw.hs.kr>
2023-06-22 05:54:30 +0900

    환자 추가 로직 수정

--- server/router/index.js
+++ server/router/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const userRouter = require('./users');
 const router = express.Router();
 const status = require('./status');
+const apply = require('./patientApply');
 
 router.use("/user", userRouter);
 router.use("/status",status );
+router.use('/apply',apply);
 
 module.exports = router;
--- server/router/patientApply/apply.js
+++ server/router/patientApply/apply.js
@@ -1,13 +1,13 @@
-const { PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const apply = async(req,res) =>{
-    try{
-        const userId = req.user.userId;
-    
-        const { useDate, whereSick, howSick} = req.body;
-    
-        const apply = await prisma.patientapply.create({
+const apply = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+
+    const { useDate, whereSick, howSick } = req.body;
+
+     const apply = await prisma.patientapply.create({
             data:{
                 serviceTime : useDate,
                 whereInfo : whereSick,
@@ -15,17 +15,35 @@ const apply = async(req,res) =>{
                 user : { connect : {userId}}
             }
         })
+    const angel = await prisma.user.findFirst({
+      where: {
+        role: "ANGEL",
+      },
+    });
 
-        return res.status(200).json({
-            message : "환자추가 성공"
-        })
-    }catch(err){
-        console.log(err);
-        return res.status(500).json({
-            message : "환자추가 실패"
-        })
-    }
-    
+    const angelId = angel.userId;
 
-}
+    const request = await prisma.request.create({
+      data: {
+        user_request_senderIdTouser: {
+          connect: { userId },
+        },
+        user_request_receiverIdTouser: {
+          connect: { userId: angelId },
+        },
+        updated_at: new Date()
+      },
+    });
+
+    return res.status(200).json({
+      message: "환자추가 성공",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "환자추가 실패",
+    });
+  }
+};
+
 module.exports = apply;
