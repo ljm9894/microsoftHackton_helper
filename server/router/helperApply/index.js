f6e9af7971d0a4e86acb399eb374e8ffc3059d02
재명 <ljm9894@dgsw.hs.kr>
2023-06-22 11:19:54 +0900

    로직 수정

--- server/router/helperApply/index.js
+++ server/router/helperApply/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const behelper = require('./behelper');
 const { authenticateUser } = require('../../middleware/authenticate');
 const getUser = require('./getUser');
+const getHelper = require('./gethelper');
 
 const router = express.Router();
 
 router.post('/behelper', authenticateUser,behelper);
 router.get('/getUser', authenticateUser,getUser );
+router.get('/getHelper/:reqestId', authenticateUser, getHelper);
 
 
 module.exports = router
--- server/router/hospital/index.js
+++ server/router/hospital/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const recHospital = require('./rechospital');
+const { authenticateUser } = require('../../middleware/authenticate');
 const router=  express.Router();
 
-router.post("/", recHospital);
+router.post("/", authenticateUser,recHospital);
 module.exports = router;
